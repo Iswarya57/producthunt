b4adb6951c0c1b9befe3a3eb0a410b75d2575d82
Iswarya57 <ishwaryaishu5798@gmail.com>
2022-09-15 10:49:36 +0530

    Products Created.

--- products/models.py
+++ products/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to="images/")
+    url  = models.URLField(max_length=200)
+    votes_total = models.IntegerField(default=1)
+    icon = models.ImageField(upload_to="images/")
+    hunter = models.ForeignKey(User,on_delete=models.CASCADE)
+
+
+    def summary(self):
+        return self.body[:105]
+
+    def date_pretty(self):
+        return self.date.strftime('%b %e %Y')
+
+    def __str__(self):
+        return self.title
+
+
+
